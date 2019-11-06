3974bd1feca5efb55688ba06f6152b885b9fc330
Tyler Mayes <tylermayes@Tylers-MacBook-Air.local>
2019-11-06 14:19:16 -0500

    updated for deployment

--- app.js
+++ app.js
@@ -12,11 +12,10 @@ app.use(express.json());
 
 app.use(require('./middleware/headers'));
 
-
 app.use('/scorestacc/user', require("./controllers/userController"));
 app.use('/scorestacc/login', require('./controllers/sessionController'));
 app.use(require('./middleware/validateSession'));
 app.use('/scorestacc/playlist', require("./controllers/playlistController"));
 app.use('/scorestacc/music', require('./controllers/scoreController'));
 
-app.listen(process.env.PORT, () => console.log(`App is listening on `));
+app.listen(process.env.PORT, () => console.log(`App is listening on 3000`));
